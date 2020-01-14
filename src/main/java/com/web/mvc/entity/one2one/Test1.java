70ef7eec67dbd7ee46cfc71dee2575ee24b9d22c
study <study@MB-207-15>
2020-01-14 21:20:08 +0800

    隨便打

---
+++ src/main/java/com/web/mvc/entity/one2one/Test1.java
@@ -0,0 +1,29 @@
+package com.web.mvc.entity.one2one;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.web.mvc.entity.JPAUtil;
+import javax.persistence.EntityManager;
+
+public class Test1 {
+    static EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+    public static void main(String[] args) throws Exception {
+        //add("老王");
+        get(1L);
+    }
+    
+    public static void add(String name) {
+        Person person = new Person();
+        person.setName(name);
+        em.getTransaction().begin();
+        em.persist(person);
+        em.getTransaction().commit();
+        System.out.println("Add OK !");
+    }
+    
+    public static void get(Long id) throws Exception {
+        Person person = em.find(Person.class, id);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(person);
+        System.out.println(json);
+    }
+}

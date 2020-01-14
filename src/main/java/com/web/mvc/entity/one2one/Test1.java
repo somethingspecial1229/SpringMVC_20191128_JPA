e4b516845e3f47e94eff10f2188bbb7b06d43cdb
study <study@MB-207-15>
2020-01-14 21:47:01 +0800

    隨便打

--- src/main/java/com/web/mvc/entity/one2one/Test1.java
+++ src/main/java/com/web/mvc/entity/one2one/Test1.java
@@ -2,15 +2,21 @@ package com.web.mvc.entity.one2one;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.web.mvc.entity.JPAUtil;
+import java.util.List;
 import javax.persistence.EntityManager;
 
 public class Test1 {
+
     static EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+
     public static void main(String[] args) throws Exception {
         //add("老王");
-        get(1L);
+        //get(1L);
+        //query();
+        //update(1L,"小美");
+        delete(1L);
     }
-    
+
     public static void add(String name) {
         Person person = new Person();
         person.setName(name);
@@ -19,11 +25,44 @@ public class Test1 {
         em.getTransaction().commit();
         System.out.println("Add OK !");
     }
-    
+
     public static void get(Long id) throws Exception {
         Person person = em.find(Person.class, id);
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(person);
         System.out.println(json);
     }
+
+    public static void query() throws Exception {
+        List<Person> list = em.createQuery("Select p From Person p", Person.class).getResultList();
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(list);
+        System.out.println(json);
+    }
+
+    public static void update(Long id, String name) throws Exception {
+        Person person = em.find(Person.class, id);
+        if (person != null) {
+            person.setName(name);
+            em.getTransaction().begin();
+            em.persist(person);
+            em.getTransaction().commit();
+            System.out.println("update OK !");
+        } else {
+            System.out.println("查無此資料");
+        }
+    }
+    
+    public static void delete(Long id) throws Exception {
+        Person person = em.find(Person.class, id);
+        if (person != null) {
+            em.getTransaction().begin();
+            em.remove(person);
+            em.getTransaction().commit();
+            System.out.println("update OK !");
+        } else {
+            System.out.println("查無此資料");
+        }
+    }
+
 }

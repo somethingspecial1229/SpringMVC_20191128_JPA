436b4519692b955e0a67867bdc1b54f28f2bd2bf
study <study@MB-207-15>
2020-02-04 20:44:18 +0800

    隨便打

---
+++ src/main/java/com/web/mvc/entity/many2many/Fund.java
@@ -0,0 +1,65 @@
+package com.web.mvc.entity.many2many;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Fund { 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    @Column
+    private String name;
+    
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "Fund_Stock",
+            joinColumns = @JoinColumn(name ="Fund_id"),
+            inverseJoinColumns = @JoinColumn(name = "Stock_id")
+    )
+    @JsonIgnoreProperties("funds")
+    private Set<Stock> stocks;
+    
+    
+    public Fund() {
+    }
+
+    public Fund(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Stock> getStocks() {
+        return stocks;
+    }
+
+    public void setStocks(Set<Stock> stocks) {
+        this.stocks = stocks;
+    }
+    
+}
---
+++ src/main/java/com/web/mvc/entity/many2many/Stock.java
@@ -0,0 +1,57 @@
+package com.web.mvc.entity.many2many;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column
+    private String symbol;
+    
+    @ManyToMany(mappedBy = "stocks")
+    @JsonIgnoreProperties("stocks")
+    private Set<Fund> funds;
+
+    public Stock(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Stock() {
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Set<Fund> getFunds() {
+        return funds;
+    }
+
+    public void setFunds(Set<Fund> funds) {
+        this.funds = funds;
+    }
+
+}   

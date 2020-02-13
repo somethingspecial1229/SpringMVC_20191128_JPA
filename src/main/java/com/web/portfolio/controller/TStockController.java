7b8818fa634e78bdfcb96a9a69e77cb1139ca5a1
study <study@MB-207-15>
2020-02-13 19:39:32 +0800

    隨便打

--- src/main/java/com/web/portfolio/controller/TStockController.java
+++ src/main/java/com/web/portfolio/controller/TStockController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/portfolio/tstock")
-public class TStockController_1 {
+public class TStockController {
 
     @PersistenceContext
     protected EntityManager em;
